4992819d5904215c22787f0ed37ff0b56db1a8a1
Muchiri-cmd <davismuchiri21@gmail.com>
2024-05-22 20:29:27 +0300

    init UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-
+import './index.css';
+/*
 function test(){
     const elem = document.createElement('div');
 
     elem.innerHTML = _.join(['Test','Successful'],' ');
-
+    
     return elem
 }
 document.body.appendChild(test());
+*/
