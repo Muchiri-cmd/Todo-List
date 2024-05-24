0a6a1c153bb4b259af63e75e2530c9f7a9e53eaf
Muchiri-cmd <davismuchiri21@gmail.com>
2024-05-24 17:53:38 +0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode:'development',
     entry:'./src/index.js',
     output : {
         filename : 'main.js',
         path : path.resolve(__dirname,'dist'),
     },
+    devtool:'inline-source-map',
+    devServer:{
+        static:'./dist',
+    },
+    module:{
+        rules : [
+            {
+                test : /\.css$/i,
+                use: ['style-loader','css-loader'],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title:'Todo App',
+            template:'./src/index.html'
+        }),
+    ],
+    output : {
+        filename: '[name].js',
+        path:path.resolve(__dirname,'dist'),
+        clean:true,
+    }
 };
